f725cfbad4589cee02a1bd1266aab528c5277c26
Krystian Kondraciuk <heliox8888@gmail.com>
2023-02-12 16:36:49 +0100

    - added config file - logic inside functions

--- python.py
+++ python.py
@@ -1,25 +1,36 @@
 import datetime
 import os
+import json
 
-absolute_path = os.path.dirname(__file__)
+def merge_videos():
+    config = read_config()
+    full_recordings_path = config["recordings_path"]
+    merged_video_path = config["merged_video_path"]
+    recordings_to_merge_text_file_path = full_recordings_path + "/files_to_merge.txt"
+    recordings_to_merge_text_file = open(recordings_to_merge_text_file_path, 'w')
 
-recordings_path = "wow_recordings"
-merged_video_path = "G:/Mój dysk/WoW recordings"
-full_recordings_path = os.path.join(absolute_path, recordings_path)
-recordings_to_merge_text_file_path = full_recordings_path + "/files_to_merge.txt"
-
-recordings_to_merge_text_file = open(recordings_to_merge_text_file_path, 'w')
-
-for file in os.listdir(full_recordings_path):
-    if(file.endswith(".mp4")):
+    for file in os.listdir(full_recordings_path):
         creation_time = os.path.getctime(full_recordings_path + "/" + file)
         dt_c = datetime.datetime.fromtimestamp(creation_time)
-        if((dt_c.date() + datetime.timedelta(days=1)) == datetime.date.today()):
-            recordings_to_merge_text_file.write("file '" + file + "'\n")
-        elif((dt_c.date() + datetime.timedelta(days=6)) < datetime.date.today()):
-            os.remove(full_recordings_path + "/" + file)
+        if(file.endswith(".json")):
+            newFileName = file.replace("'", " ")
+            os.rename(full_recordings_path + "/" + file, full_recordings_path + "/" + newFileName)
+            if((dt_c.date() + datetime.timedelta(days=6)) < datetime.date.today()):
+                os.remove(full_recordings_path + "/" + newFileName)
+        if(file.endswith(".mp4")):
+            newFileName = file.replace("'", " ")
+            os.rename(full_recordings_path + "/" + file, full_recordings_path + "/" + newFileName)
+            if((dt_c.date() + datetime.timedelta(days=1)) == datetime.date.today()):
+                recordings_to_merge_text_file.write("file '" + newFileName + "'\n")
+            elif((dt_c.date() + datetime.timedelta(days=6)) < datetime.date.today()):
+                os.remove(full_recordings_path + "/" + newFileName)
 
-recordings_to_merge_text_file.close()
+    recordings_to_merge_text_file.close()
 
-os.system('cmd /c "ffmpeg -f concat -safe 0 -i ' + recordings_to_merge_text_file_path + ' -c copy "' + merged_video_path + '/output.mp4"')
+    os.system('cmd /c "ffmpeg -f concat -safe 0 -i ' + recordings_to_merge_text_file_path + ' -c copy "' + merged_video_path + '/' + str(datetime.date.today()) + '.mp4"')
     
+def read_config():
+    with open("config.json", "r") as f:
+        return json.load(f)
+
+merge_videos()
